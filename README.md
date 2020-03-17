fb4767d2f6c390f7c4839926d792211a2c421239
ABHILASH <ss.abhilash3@gmail.com>
2020-03-17 23:19:52 +0530

    wip

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # react-native-select-input
+
 select-input-dropdown for react native
+
+-npm/yarn package for select input in react-native application
+
+-development is ongoing
--- src/Select.js
+++ src/Select.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TextInput } from 'react-native';
 
 function Select() {
   return (
     <View>
       <Text>Select</Text>
+      <TextInput />
     </View>
   );
 }
