bdbc4306885d6560a1b7b154d2bc52551dc3555e
ABHILASH <ss.abhilash3@gmail.com>
2020-03-18 23:20:56 +0530

    add flat list for listing select options

--- src/Select.js
+++ src/Select.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { View, Text, TextInput } from 'react-native';
+import { View, Text, TextInput, FlatList } from 'react-native';
 
 function Select() {
+  const options = ['item1', 'item2', 'item3', 'item4', 'item5'];
+  // renderItem = ({ item }) => (<View key={item.key}><Text>{item.title}</Text></View>);
+  const renderItem = ({ item }) => (
+    <View key={item.key}>
+      <Text>{item.title}</Text>
+    </View>
+  );
   return (
     <View>
       <Text>Select</Text>
       <TextInput />
+      <FlatList data={options} renderItem={renderItem} />
     </View>
   );
 }
